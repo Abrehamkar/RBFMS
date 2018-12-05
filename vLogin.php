fb8ab459c6ba5f80f9d426cc75be0feafe5faef5
DESKTOP-BEU9MD2\HMZ <monosoft54@gmail.com>
2018-12-06 04:07:55 +0500

    vRegisteration Updated
    Add a 'Verified' field in vthe 'vendors' table in the 'rbfms' database.

--- vLogin.php
+++ vLogin.php
@@ -10,14 +10,15 @@ $exists = 0;
 $query = "SELECT * FROM `vendors` WHERE `Phone Number`='$phoneNumber' AND `Password`='$password'";
 $result = mysqli_query($conn, $query);
 $row = mysqli_fetch_array($result);
+$verified = $row['Verified'];
 $count = mysqli_num_rows($result);
 
 if($count >= 1) {
-    $exits = 1;
+    $exists = 1;
 }
 
 $message = "";
-if($exists == 1) {
+if($exists == 1 && $verified == 1) {
     $message = "Success";
 }
 else {
--- vRegistration.php
+++ vRegistration.php
@@ -9,9 +9,14 @@ $phoneNumber = $_POST['phoneNumber'];
 $cnic = $_POST['cnic'];
 $skill = $_POST['skill'];
 $password = $_POST['password'];
+$source = $_POST['source']; 
 
-$query = "INSERT INTO `vendors`(`First Name`, `Last Name`, `Date of Birth`, `Phone Number`, `CNIC`, `Skill`, `Password`) VALUES ('$firstName', '$lastName', '$dob', '$phoneNumber', '$cnic', '$skill', '$password')";
-$result = mysqli_query($conn, $query);
+if($source == "unity.vendorapp")
+{
+    $verified = 0;
+    $query = "INSERT INTO `Vendors`(`First Name`, `Last Name`, `Date of Birth`, `Phone Number`, `CNIC`, `Skill`, `Password`, `Verified`) VALUES ('$firstName', '$lastName', '$dob', '$phoneNumber', '$cnic', '$skill', '$password', '$verified')";
+    $result = mysqli_query($conn, $query);
+}
 
 $message = "";
 if($result == '1')
